91ed238e30a5fde2e714d8d0d8d316343dfcacdb
Phil Hawksworth <phawksworth@gmail.com>
2013-01-22 23:26:46 +0000

    keyboard events and push/popstate handling

--- src/js/statification.js
+++ src/js/statification.js
@@ -1,4 +1,75 @@
+---
+---
+
+var stftn = {};
+stftn.slides = [];
+stftn.currentSlide = -1;
+{% for slide in site.posts reversed %} stftn.slides.push('{{ slide.id }}'); {% endfor %}
+
+
 // giddy up.
 $(function() {
-	console.log("let's go!");
+    stftn.init();
 });
+
+
+stftn.init = function() {
+    stftn.addEventListners();
+};
+
+
+stftn.addEventListners = function() {
+
+    // control slides with keys
+    $(document).on( "keypress", function(event){
+        var action = {
+            32: stftn.next, //space
+            33: stftn.next,
+            44: stftn.prev
+        };
+        console.log("key", event.which);
+        if(action[event.which]) {
+            action[event.which].call();
+        }
+    });
+
+    // listen for broswer forwd/back buttons
+    window.onpopstate = function(event){
+        var url = window.location.pathname;
+        $('.stage').load(url +' .section');
+    };
+};
+
+
+stftn.next = function() {
+    stftn.currentSlide++;
+    if(stftn.currentSlide >= stftn.slides.length) {
+        stftn.currentSlide = stftn.slides.length - 1;
+        return;
+    }
+    stftn.getSlide(stftn.currentSlide);
+};
+
+
+stftn.prev = function() {
+    stftn.currentSlide--;
+    if(stftn.currentSlide <= -1) {
+        stftn.currentSlide = - 1;
+        return;
+    }
+    stftn.getSlide(stftn.currentSlide);
+};
+
+
+stftn.getSlide = function(i) {
+
+    var url = "/" + stftn.slides[i];
+    if (history.pushState ) {
+        $('.stage').load(url +' .section');
+        history.pushState(undefined, undefined, url);
+    } else {
+        window.location.pathname = url;
+    }
+
+};
+
